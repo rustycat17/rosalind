159f58472b7cdd75d3be9e3f74a74c843c5f0dfa
rustycat17 <rustycat17@posteo.nz>
2021-03-25 20:27:06 +0100

    implemented DNA algorithm

--- bioinformatics_stronghold/DNA/src/main.rs
+++ bioinformatics_stronghold/DNA/src/main.rs
@@ -1,3 +1,24 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let filepath = "data/rosalind_dna.txt";
+    let input = fs::read_to_string(filepath);
+    let input = match input {
+        Ok(input) => input,
+        Err(err) => panic!("cannot read input file, error message {}", err),
+    };
+
+    let mut output = (0, 0, 0, 0);
+
+    for symbol in input.chars() {
+        match symbol {
+            'A' => output.0 += 1,
+            'C' => output.1 += 1,
+            'G' => output.2 += 1,
+            'T' => output.3 += 1,
+            _ => panic!("unknown character {}", symbol),
+        }
+    }
+
+    println!("{} {} {} {}", output.0, output.1, output.2, output.3);
 }
