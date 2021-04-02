97dbafdcec9f40dbe6130f011ef8ff423fb938ce
rustycat17 <rustycat17@posteo.nz>
2021-04-02 19:22:28 +0200

    added first implementation for HAMM

--- bioinformatics_stronghold/HAMM/src/main.rs
+++ bioinformatics_stronghold/HAMM/src/main.rs
@@ -1,3 +1,24 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let filepath = "data/input.txt";
+    let input = fs::read_to_string(filepath);
+    let input = match input {
+        Ok(input) => input,
+        Err(err) => panic!("{}", err),
+    };
+
+    let out:Vec<&str> = input.split("\n").collect();
+    let sequence1 = out[0];
+    let sequence2 = out[1];
+
+    let mut counter = 0;
+    for i in sequence1.chars().zip(sequence2.chars()) {
+        let (j, k) = i;
+        if j != k {
+            counter += 1;
+        }
+    }
+
+    println!("{}", counter);
 }
