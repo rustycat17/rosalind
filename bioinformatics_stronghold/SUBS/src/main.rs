45479a3b8351fb712b1e9dcbe133c1a083fdfcef
rustycat17 <rustycat17@posteo.nz>
2021-04-02 10:55:14 +0200

    added implementation for SUBS project

--- bioinformatics_stronghold/SUBS/src/main.rs
+++ bioinformatics_stronghold/SUBS/src/main.rs
@@ -1,3 +1,29 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let filepath = "data/rosalind_subs.txt";
+    let input = fs::read_to_string(filepath);
+
+    let input = match input {
+        Ok(input) => input,
+        Err(err) => panic!("{}", err),
+    };
+
+    let out:Vec<&str> = input.split("\n").collect();
+    let sequence = out[0];
+    let pattern = out[1];
+    let mut res:Vec<usize> = vec![];
+
+    let mut offset = 0;
+    loop {
+        let index = sequence[offset..].find(pattern);
+        let matched_index = match index {
+            Some(matched_index) => matched_index + 1 + offset,
+            None => break,
+        };
+        offset = matched_index;
+        res.push(offset);
+    }
+
+    println!("{:?}", res);
 }
